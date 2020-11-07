b5ee6ee45dbc5155dc623bbf5c012a188afd5aa9
Joseph Fairbanks <joebanks555@gmail.com>
2020-11-07 11:13:28 -0700

    Starting Home Page and Browse

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Browse from '../views/Browse.vue'
 
 Vue.use(VueRouter)
 
@@ -12,9 +12,9 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: '/browse',
+    name: 'Browse',
+    component: Browse
   },
 ]
 
