ef20587510ce82df6bc2a54e3a3322b75dfd762f
Joseph Fairbanks <joebanks555@gmail.com>
2020-11-07 13:18:51 -0700

    Finished Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,32 @@
 <div>
   <h1>Welcome to Board Game Bonanza</h1>
   <img class = "home" src= "\images\home.jpg">
+  <div class = "description">
+    <p>Board Game Bonanza is a world-renowned board game vendor. We offer a wide
+    variety of board games from classics , to fantasy games, to card games and much much more!
+    Browse our inventory, read board games reviews, and contact us using the links below.<br><br>
+    Thank you for choosing <strong>Board Game Bonanza</strong></p>
+  </div>
+  <section class="services-grid">
+    <div class="box">
+      <router-link to="/browse">
+      <img class = "image" src="/images/browse.jpg" alt="Browse">
+      <h6>Browse</h6>
+      </router-link>
+    </div>
+    <div class="box">
+      <router-link to="/reviews">
+      <img class = "image" src="/images/review.jpg" alt="Reviews">
+      <h6>Reviews</h6>
+      </router-link>
+    </div>
+    <div class="box">
+      <router-link to="/contact">
+      <img class = "image" src="/images/contact.jpg" alt="Contact Us">
+      <h6>Contact Us</h6>
+      </router-link>
+    </div>
+  </section>
 </div>
 </template>
 
@@ -16,14 +42,68 @@ h1 {
   height: auto;
   width: 75%;
 }
+.description {
+  font-family: "Century Gothic", CenturyGothic, Geneva, AppleGothic, sans-serif;
+  background-color: #F6F6F6;
+  border: 5px solid #000000;
+  text-align: center;
+  margin: 30px;
+  font-size: 18pt;
+  padding-left: 5px;
+  padding-right: 5px;
+}
+
+.box{
+  position: relative;
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+
+.services-grid {
+  display: grid;
+  grid-template: auto auto auto auto auto / 1fr 1fr 1fr;
+  grid-column-gap: 15px;
+  grid-row-gap: 15px;
+  margin-right: 20px;
+  margin-left: 20px;
+}
+
+.image {
+  width: 100%;
+}
 
 @media only screen and (max-width: 990px) {
   h1 {
     font-size: 20pt;
   }
+  h6 {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 900;
+  color: white;
+  text-transform: uppercase;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
 }
 
 @media only screen and (min-width: 991px) {
-
+h6 {
+font-family: 'Roboto', sans-serif;
+font-weight: 900;
+color: white;
+text-transform: uppercase;
+margin: 0;
+position: absolute;
+top: 50%;
+left: 50%;
+font-size: 2rem;
+transform: translate(-50%, -50%);
+text-align: center;
+}
 }
 </style>
