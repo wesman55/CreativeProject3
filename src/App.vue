d74f523a2e019d3991b2a14b9847e0444f4bfa63
Joseph Fairbanks <joebanks555@gmail.com>
2020-11-07 10:30:02 -0700

    Added logo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">Cart</router-link> | 
-      <router-link to="/reviews">Reviews</router-link>
-    </div>
-    <router-view/>
+  <div id="menu">
+  <div id="brand">
+    <router-link to="/">
+      <img src="../images/logo.png">
+    </router-link>
   </div>
+  <div id="side">
+    <router-link to="/browse">
+      <div class="menu-item browse">
+        <img src="../images/review.png">
+        <p>Reviews</p>
+      </div>
+    </router-link>
+    <router-link to="/cart">
+      <div class="menu-item">
+        <img src="../images/cart.png">
+        <p>{{ numberOfItems }} items</p>
+      </div>
+    </router-link>
+  </div>
+</div>
+</div>
 </template>
 
 <style>
@@ -29,5 +44,86 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 50px 100px;
+
+}
+
+#menu {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 5px;
+  grid-template-areas: "none brand side";
+  margin-bottom: 50px;
+}
+
+#menu a {
+  color: #B84901;
+}
+
+#brand {
+  grid-area: brand;
+  display: flex;
+  justify-content: center;
+}
+
+#brand img {
+  height: 200px;
+}
+
+#side {
+  grid-area: side;
+  display: flex;
+  justify-content: flex-end;
+}
+
+#side img {
+  width: 50px;
+}
+
+.menu-item {
+  display: flex;
+  flex-direction: column;
+}
+
+.menu-item p {
+  margin: 0px;
+}
+
+.browse {
+  margin-right: 50px;
+}
+
+.footer {
+  border-top: 3px solid #000000;
+  text-align: center;
+  margin-top: 150px;
+}
+
+#menu2 {
+  list-style-type: none;
+}
+
+a {
+  display: block;
+}
+
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 8px 10px;
+  text-decoration: none;
+  font-size: 12pt;
+  margin-right: 30px;
+}
+
+li a:hover {
+  background-color: #C0C0C0;
 }
 </style>
