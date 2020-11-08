e2f13096cb2a33e46eac44d0efe4d453b94049a7
Joseph Fairbanks <joebanks555@gmail.com>
2020-11-07 22:10:19 -0700

    contact page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Browse from '../views/Browse.vue'
 import Cart from '../views/Cart.vue'
+import Contact from '../views/Contact.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/cart',
     name: 'Cart',
     component: Cart
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
   },
 ]
 
---
+++ src/views/Contact.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <h1>Contact Us!</h1>
+  <img class = "home" src= "\images\footer.jpg">
+  <div class = "description">
+    <h2> Have a Question about a product? Need a refund? <br><br>Let us Know!</h2>
+    <p>Board Game Bonanza is renowned for offering high quality products and low prices.
+    If you would like to talk to a representative, ask a question, scedule a delivery, or
+    just want to chat call us at <strong> 602-791-GAME </strong> or email us at
+    <strong> boardgamesbonanza@gmail.com </strong> </p>
+  </div>
+  <section class="services-grid">
+    <div class="box">
+      <router-link to="/browse">
+      <img class = "image" src="/images/browse.jpg" alt="Browse">
+      <h6>Browse</h6>
+      </router-link>
+    </div>
+    <div class="box">
+      <router-link to="/reviews">
+      <img class = "image" src="/images/review.jpg" alt="Reviews">
+      <h6>Reviews</h6>
+      </router-link>
+    </div>
+    <div class="box">
+      <router-link to="/cart">
+      <img class = "image" src="/images/cart.jpg" alt="Cart">
+      <h6>View Cart</h6>
+      </router-link>
+    </div>
+  </section>
+  <img class = "home" src= "\images\heading.jpg">
+</div>
+</template>
